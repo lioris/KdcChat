ad91dcaaafd1d36cb4f06d0eaa759697a0f04d06
lioris <liorisraelov@gmail.com>
2018-06-21 21:27:32 +0300

    add ftp service

---
+++ Contracts/IFtpService.cs
@@ -0,0 +1,15 @@
+﻿using System.ServiceModel;
+
+namespace Contracts
+{
+    //[ServiceContract(CallbackContract = typeof(IClientCallBack))]
+    [ServiceContract]
+    public interface IFtpService
+    {
+        [OperationContract]
+        void printKoko();
+
+       
+
+    }
+}
---
+++ client/FtpProxy.cs
@@ -0,0 +1,43 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class FtpProxy
+    {
+        private ChannelFactory<IFtpService> channel;
+        private IFtpService proxy;
+        #region Singelton
+        private static FtpProxy _instance;
+
+        private FtpProxy()
+        {
+            // creat a channel to comunicate with the game server
+            channel = new ChannelFactory<IFtpService>("FtpServiceEndpoint");
+            proxy = channel.CreateChannel();
+        }
+
+        public static FtpProxy Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new FtpProxy();
+
+                return _instance;
+            }
+        }
+        #endregion
+
+        internal IFtpService GetFtpProxy()
+        {
+            return proxy;
+        }
+    }
+}
+
