c145116c74e90b5aac1677f69d4f0c23febaf46e
fayakov <fayakov@gmail.com>
2018-06-21 21:42:11 +0300

    add session class

---
+++ client/resources/clientAllData.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using client.resources;
+using LinqToSql;
+
+namespace client
+{
+    class clientAllData
+    {
+        //private DuplexChannelFactory<IKdcService> channel;
+        //private IKdcService proxy;
+        private User m_myUser;
+        private List<string> m_partnerUser;
+        private Dictionary<string, session> m_partnerSession;
+        #region Singelton
+        private static clientAllData _instance;
+
+        private clientAllData()
+        {
+            m_myUser = new User();
+            m_partnerUser = new List<string>();
+            m_partnerSession = new Dictionary<string, session>();
+        }
+
+        public static clientAllData Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new clientAllData();
+
+                return _instance;
+            }
+        }
+        #endregion
+
+
+        internal User getMyClient()
+        {
+            return m_myUser;
+        }
+
+        internal List<string> getAllPartnersUsers()
+        {
+            return m_partnerUser;
+        }
+
+        internal void addUserToPartnerUsers(string partnerUsername)
+        {
+            if(!m_partnerUser.Contains(partnerUsername))
+            {
+                m_partnerUser.Add(partnerUsername);
+            }
+        }
+
+        internal void addNewSession(session sessionData, string username)
+        {
+            if(!m_partnerSession.ContainsKey(username))
+            {
+                m_partnerSession.Add(username, sessionData);
+            }
+        }
+
+        internal session getSession( string username)
+        {
+            return m_partnerSession[username];
+        }
+
+    }
+}
---
+++ client/resources/session.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client.resources
+{
+    public class session
+    {
+        private byte[] m_sessionKye;
+        public session(byte[] sessionKey)
+        {
+            m_sessionKye = sessionKey;
+        }
+    }
+}
