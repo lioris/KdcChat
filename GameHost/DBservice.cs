2d280cc4294be214f1d5477d139eb6ce70974518
lioris <liorisraelov@gmail.com>
2018-06-22 18:08:30 +0300

    refactor solution

---
+++ GameHost/DBservice.cs
@@ -0,0 +1,104 @@
+﻿using Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinqToSql;
+using System.Threading;
+
+namespace KdcService
+{
+
+    public class DBservice
+    {
+        pokerDataBaseDataContext dbContext = new pokerDataBaseDataContext();
+
+        public bool isUserExsist(string Name)
+        {
+            try
+            {
+                User exsistUser = dbContext.Users.Single(user => user.Name == Name);
+
+                Console.WriteLine("user with name " + Name + " is exsists");
+                return true;
+            }
+            catch
+            {
+                Console.WriteLine("user with name " + Name + " not exsists");
+                return false;
+            }
+        }
+
+        
+
+        public User login(string Name, string password)
+        {
+            Console.WriteLine("trying to login with name = {0} and password = {1}", Name, password);
+            try
+            {
+                User exsistUser = dbContext.Users.Single(user => user.Name == Name);
+                return exsistUser;
+            }
+            catch
+            {
+                Console.WriteLine("invaild username or password  " + Name + " " + password);
+                return null;
+            }
+        }
+
+        public User Register(string Name, string Password)
+        {
+            Console.WriteLine("trying to register with name = {0} and password = {1}", Name, Password);
+
+            if (isUserExsist(Name))
+                return null;
+
+            User newUser = new User
+            {
+                Name = Name,
+                PassWord = Password,
+                Email = "no mail",
+                
+            };
+
+            dbContext.Users.InsertOnSubmit(newUser);
+
+            try
+            {
+                dbContext.SubmitChanges();
+                Console.WriteLine("submited");
+                return newUser;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("somthing went wrong with the conecrtion");
+                return null;
+            }
+        }
+        
+        public User getUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User getUserByName(string name)
+        {
+            try
+            {
+                User exsistUser = dbContext.Users.Single(user => user.Name == name);
+
+                Console.WriteLine("user with name " + name + " is exsists");
+                return exsistUser;
+            }
+            catch
+            {
+                Console.WriteLine("user with name " + name + " not exsists");
+                return null;
+            }
+        }
+        
+
+        
+    }
+}
---
+++ GameHost/KdcService.cs
@@ -0,0 +1,138 @@
+﻿using common;
+using Contracts;
+using LinqToSql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace KdcService
+{
+    //[ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    public class KdcService : IKdcService
+    {
+        public static Dictionary<string, IClientKdcCallBack> user_list = new Dictionary<string, IClientKdcCallBack>();
+        DBservice m_DBservice = new DBservice();
+
+        KdcService()
+        {
+        }
+
+        public CSessionKeyResponse GetSessionKey(CSessionParams sessionParams)
+        {
+
+            User retUser1FromDB = m_DBservice.getUserByName(sessionParams.client1UserName);
+            User retUser2FromDB = m_DBservice.getUserByName(sessionParams.client2UserName);
+
+            if(retUser1FromDB == null || retUser2FromDB == null)
+            {
+                if(retUser1FromDB == null)
+                {
+                    Console.Write(sessionParams.client1UserName + "not exist in DB");
+                }
+                if (retUser2FromDB == null)
+                {
+                    Console.Write(sessionParams.client2UserName + "not exist in DB");
+                }
+                return null;
+            }
+
+            byte[] key = common.CAes.NewKey();
+
+            byte[] keyB = common.CAes.SimpleEncryptWithPassword(key, retUser2FromDB.PassWord);
+            byte[] keyA = common.CAes.SimpleEncryptWithPassword(key, retUser1FromDB.PassWord);
+            byte[] keyAB = common.CAes.SimpleEncryptWithPassword(keyB, retUser1FromDB.PassWord);
+
+            CSessionKeyResponse retVal = new CSessionKeyResponse();
+            retVal.m_sessionKeyA = keyA;
+            retVal.m_sessionKeyB = keyAB;
+            return retVal;
+        }
+
+        public User LogInApp(string userName)
+        {
+            if (user_list.ContainsKey(userName))
+            {
+                Console.WriteLine("you already logged");
+                return null;
+            }
+
+            Console.WriteLine("LOGIN: NAME {0} ", userName);
+
+            User msgKdcToClientLoggin = null;
+
+            //msgKdcToClientLoggin.Name = common.CAes.SimpleEncryptWithPassword(userName, password1);
+            //msgKdcToClientLoggin.PassWord = common.CAes.SimpleEncryptWithPassword(password1, password1);
+
+            User retUserFromDB = m_DBservice.getUserByName(userName);
+            if (retUserFromDB != null)
+            {
+                msgKdcToClientLoggin = new User();
+                byte[] key = common.CAes.NewKey();
+                msgKdcToClientLoggin.Name = common.CAes.SimpleEncryptWithPassword(userName, retUserFromDB.PassWord);
+                msgKdcToClientLoggin.PassWord = common.CAes.SimpleEncryptWithPassword(retUserFromDB.PassWord, retUserFromDB.PassWord);
+                user_list.Add(userName, OperationContext.Current.GetCallbackChannel<IClientKdcCallBack>());
+                sendNewUserToAllClients(userName, 1000);
+            }
+            
+            return msgKdcToClientLoggin;
+        }
+
+        public void sendNewUserToAllClients(string userName, int port)
+        {
+            List<string> nameList = new List<string>();
+            foreach (KeyValuePair<string, IClientKdcCallBack> client in user_list)
+            {
+                nameList.Add(client.Key);
+            }
+
+            foreach (KeyValuePair<string, IClientKdcCallBack> client in user_list)
+            {
+                client.Value.addNewConnectedUser(userName, nameList, port);
+            }
+        }
+
+        public List<string> getAllConnectedUsers()
+        {
+            List<string> nameList = new List<string>();
+            foreach (KeyValuePair<string, IClientKdcCallBack> client in user_list)
+            {
+                nameList.Add(client.Key);
+            }
+            return nameList;
+        }
+
+        public void LogOutApp(string name)
+        {
+            if (user_list.ContainsKey(name))
+                user_list.Remove(name);
+        }
+
+        public User RegisterApp(string userName, string Password)
+        {
+
+
+            return null;
+        }
+
+        //public void sendMassage(int tableid, string massage)
+        //{
+
+        //    foreach (KeyValuePair<int, IClientCallBack> client in tables_lists[tableid].playing_list)
+        //    {
+        //        client.Value.updateChat(massage);
+        //    }
+
+        //    foreach (var watching in tables_lists[tableid].watching_list)
+        //    {
+        //        watching.updateChat(massage);
+        //    }
+
+        //}
+    }
+}
